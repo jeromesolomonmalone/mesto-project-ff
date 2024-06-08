9de0960386b7f1593159972e7b2e293bcced6e2d
Roman Pigunov <jerome.malone@yandex.ru>
2024-06-08 17:48:30 +0300

    Выполнил проектную работу

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
-
+const cardsList = document.querySelector(".places__list");
 // @todo: Функция создания карточки
-
+function addCard(item, deleteCard) {
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  cardElement.querySelector(".card__image").src = item.link;
+  cardElement.querySelector(".card__image").alt = item.name;
+  cardElement.querySelector(".card__title").textContent = item.name;
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", deleteCard);
+  return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(evt) {
+  evt.target.closest(".places__item").remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (element) {
+  cardsList.append(addCard(element, deleteCard));
+});
